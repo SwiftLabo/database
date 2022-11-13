72374f8f5e9f3bbbfc3c09407308acf52ab7fca0
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-11-13 18:45:39 +0700

    13/11/22 18.45

--- start.sh
+++ start.sh
@@ -9,6 +9,27 @@ function relistdevice(){
 function createnewdevice(){
     if [ -z $1 ] 
     then
-        echo lul
+      echo "Error: Please Specify The Device Name"
+      return
     fi
+        if [ -z $2 ] 
+    then
+      echo "Error: Please Specify The Device Codename"
+      return
+    fi
+            if [ -z $3 ] 
+    then
+      echo "Error: Please Specify The Device Maintainer"
+      return
+    fi
+                if [ -z $3 ] 
+    then
+      echo "Error: Please Specify The Device Pictures"
+      return
+    fi
+    Name=$1
+    Codename=$2
+    Maintainer=$3
+    Pictures=$4
+    printf "{\n    \"Name\": \"$Name\",\n    \"Codename\": \"$Codename\",\n    \"Maintainer\": \"$Maintainer\",\n    \"Pictures\": \"image/$Pictures\",\n    \"Filename\": \"$Filename\",\n    \"BuildDate\": \"$BuildDate\",\n    \"Android\": \"13\",\n    \"Download\": \"$Download\",\n    \"isAvailable\": \"$Download\"\n}" > sa
 }
